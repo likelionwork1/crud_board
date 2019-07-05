0466c1a1d6e8c8727e3e864856264f884cb828d7
Ubuntu <ubuntu@ip-172-31-24-127.us-east-2.compute.internal>
2019-07-05 18:19:18 +0000

    initialize controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'homes/index'
+  get 'homes/create'
+  get 'homes/show'
+  get 'homes/edit'
+  get 'homes/update'
+  get 'homes/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
